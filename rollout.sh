8058562f7a7c955601dbce62e615e078bb0f4dc2
Sebastian Winkler <sebastian.winkler@slmn.de>
2018-12-22 15:20:36 +0100

    fixes rollout.sh

--- rollout.sh
+++ rollout.sh
@@ -6,8 +6,10 @@ curl -X GET -H "Content-Type: application/json" \
     -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" \
     "https://api.digitalocean.com/v2/kubernetes/clusters/$KUBERNETES_CLUSTER/kubeconfig" > .kubeconfig
 
+SHA=$(git rev-parse --short HEAD)
+
 kubectl --kubeconfig ./.kubeconfig patch deployment github-actions-test-deployment \
-    -p"{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"github-actions-test\",\"image\":\"sekl/github-actions-test:$IMAGE_SHA\"}]}}}}"
+    -p"{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"github-actions-test\",\"image\":\"sekl/github-actions-test:$SHA\"}]}}}}"
 
 kubectl --kubeconfig ./.kubeconfig rollout status deployment.v1.apps/github-actions-test-deployment
 
